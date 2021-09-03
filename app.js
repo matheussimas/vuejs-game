21f156527216871c1f19a7a2a88fad47a9474aa3
unknown <matheussimasalves@gmail.com>
2021-09-02 23:13:28 -0300

    atack on player implemented

--- app.js
+++ app.js
@@ -15,15 +15,33 @@ new Vue({
 
     methods: {
         startGame() {
-            this.running = !this.running
+            this.running = true
             this.monsterLife = 100
             this.playerLife = 100
         },
+        funcAtack(special) {
+           if (special == false) {
+               this.hurt(7, 12, false)
+           } else {this.hurt(8, 16, true)}
+        },
+
+        hurt(min, max, special) {
+            const plus = special ? 5 : 0
+            const hurt = this.getRandom(min + plus, max + plus)
+            this.playerLife = Math.max(this.playerLife - hurt, 0)
+        },
+
+        getRandom(min, max) {
+            const value = Math.random() * (max - min) + min
+            return Math.round(value)
+        }
 
     },
 
     watch: {
-
+        hasResult(value) {
+            if (value) this.running = false
+        }
     }
 
 })
